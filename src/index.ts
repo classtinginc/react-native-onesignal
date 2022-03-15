bc8cb4c7598d54f5727ff7e3a20afbd0c3c8ec58
leegeunhyeok <dev.ghlee@gmail.com>
2022-03-15 18:19:55 +0900

    fix: init 메소드 타입 & 로직 추가
    init 메소드가 누락되어 추가함

--- src/index.ts
+++ src/index.ts
@@ -790,6 +790,14 @@ export default class OneSignal {
     /**
      * Classting custom methods
      */
+    static init (handler: (success: boolean) => void): void {
+        if (Platform.OS === 'android') {
+            RNOneSignal.init(handler);
+        } else {
+            handler(true);
+        }
+    }
+
     static getSdkVersion(): string {
         return RNOneSignal.getSdkVersion();
     }
